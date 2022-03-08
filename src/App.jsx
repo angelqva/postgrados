907634777b3fd861544e186c1926629757e279e9
Angel Nápoles Nápoles <angel.napolesqva@gmail.com>
2022-03-08 13:20:02 -0500

    fuentes offline para material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <Fragment>
       <h1>Postgrados</h1>
+      <Button variant="contained">Hello World</Button>
     </Fragment>
   );
 }
